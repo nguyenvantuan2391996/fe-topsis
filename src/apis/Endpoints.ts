f62778c2db4c43bcd36a41081ea4445442c35a27
tuannguyen <tuan.nguyen25@tiki.vn>
2023-11-05 15:07:24 +0700

    feat: updating end point

--- src/apis/Endpoints.ts
+++ src/apis/Endpoints.ts
@@ -1,5 +1,5 @@
-// export const END_POINT = "https://be-topsis.herokuapp.com/v1/api";
-export const END_POINT = "http://localhost:3000/v1/api";
+export const END_POINT = "https://be-project-prod.up.railway.app/v1/api";
+// export const END_POINT = "http://localhost:3000/v1/api";
 export const USERS_POINT = "users";
 export const STANDARDS_POINT = "standards";
 export const SCORE_RATING_POINT = "score-ratings";
